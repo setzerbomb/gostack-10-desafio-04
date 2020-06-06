13168e58824c347d096dd284717ecf3ca74c81b8
Set <6942774+setzerbomb@users.noreply.github.com>
2020-06-06 01:20:02 -0300

    react app backbone

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+function Main() {
+  return (
+    <Container>
+      <h1>It Works</h1>
+    </Container>
+  );
+}
+
+export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 85%;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  margin: 80px auto;
+
+  h1 {
+    text-align: center;
+  }
+`;
